bd35371f4c64dcbfd8bbaf4b278172887987f0af
Muhammad Kamran <me@mkamran.us>
2022-11-21 15:18:02 -0800

    Math class, Gui input, and prints

---
+++ Main.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+// import java.util.Scanner;
+// import javax.swing.JOptionPane;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    // int x = 123; // init
+    // double y = 3.15;
+    // boolean z = true;
+
+    // if (z) {
+    // System.out.println(x);
+    // System.out.println(y);
+    // }
+
+    // Getting user inout
+    // Scanner scanner = new Scanner(System.in);
+
+    // System.out.println("What is your name?");
+    // String name = scanner.nextLine();
+    // System.out.println("How old are you?");
+    // int age = scanner.nextInt();
+    // scanner.nextLine(); // To clear out the scanner
+    // System.out.println("What is your fave food?");
+    // String food = scanner.nextLine();
+    // System.out.println("Your name is : " + name + "\nage : " + age + "\nFood: " +
+    // food);
+
+    // scanner.close();
+    // Bit of GUI
+    // String name = JOptionPane.showInputDialog("What's your name?");
+
+    // int age = Integer.parseInt(JOptionPane.showInputDialog("What's your age?"));
+    // double height = Double.parseDouble(JOptionPane.showInputDialog("What's your
+    // height?"));
+
+    // JOptionPane.showMessageDialog(null, name + " your age is " + age + "you're "
+    // + height + "CM tall");
+
+    // Math Class
+
+    // double x = 3.14, y = -10;
+
+    // double z = Math.max(x, y);
+
+    // System.out.println(z);
+
+    // Get the Hypotenuse of the triangle
+
+    // double x = 0, y = 0, z = 0;
+
+    // Scanner scanner = new Scanner(System.in);
+
+    // System.out.println("Enter side x: ");
+    // x = scanner.nextDouble();
+    // System.out.println("Enter side y: ");
+    // y = scanner.nextDouble();
+
+    // z = Math.sqrt((x * x) + (y * y));
+
+    // System.out.println("Hypotenuse is : " + z);
+
+    // scanner.close();
+
+  }
+
+}
