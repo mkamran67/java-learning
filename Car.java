a3919861a9b8471d41d3a73114009302e275c0b3
Muhammad Kamran <me@mkamran.us>
2022-11-26 23:25:25 -0800

    objects

---
+++ Car.java
@@ -0,0 +1,11 @@
+
+public class Car {
+
+  String make = "toyota";
+  String model = "prius";
+
+  public void printDeets() {
+    System.out.println(make + "\t" + model);
+  }
+
+}
---
+++ Human.java
@@ -0,0 +1,22 @@
+public class Human {
+
+  String name;
+  int age;
+  double weight;
+
+  Human() {
+
+  }
+
+  Human(String name, int age, double weight) {
+    this.name = name;
+    this.age = age;
+    this.weight = weight;
+  }
+
+  void printDeets() {
+    System.out.println(this.name);
+    System.out.println(this.age);
+    System.out.println(this.weight);
+  }
+}
