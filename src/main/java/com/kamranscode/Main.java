c8d6b427d7d0f7fb8b33bcbdd62b54e7bad02985
Muhammad Kamran <mkamran1967@gmail.com>
2022-12-02 19:03:17 -0800

    init

---
+++ src/main/java/com/kamranscode/Main.java
@@ -0,0 +1,24 @@
+package com.kamranscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public GreetResponse greet() {
+        return new GreetResponse("hello world!");
+    }
+
+    record GreetResponse(String message) {
+
+    }
+
+}
